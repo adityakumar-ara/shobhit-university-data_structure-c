a977cd098b018d282831c026591f9406927809d3
Aditya Kumar <adityaara7667@gmail.com>
2026-02-02 22:57:09 +0530

    add more

---
+++ first_str.c
@@ -0,0 +1,34 @@
+// print array elements
+
+
+// #include<stdio.h>
+// int main(){
+    // int arr[20],i,n;
+    // printf("enter number of element");
+    // scanf("%d",&n);
+    // for ( i = 0; i < n; i++)
+    // {
+    //     printf("enter element");
+    //     scanf("%d",&arr[i]);
+    // }
+    // printf("------element are---------");
+    // for ( i = 0; i < n; i++)
+    // {
+    //     printf("%d",arr[i]); 
+    // }
+    // ----------------------------------------------------------------------------------
+//     int arr[20],n,i;
+//     printf("enter number of element");
+//     scanf("%d",&n);
+//     for ( i = 0; i < n; i++)
+//     {
+//         printf("enter elements:");
+//         scanf("%d",&arr[i]);
+//     }
+//      printf("elements are------");
+//      for ( i = 0; i < n; i++)
+//      {
+//         printf("%d",arr[i]);
+//      }
+// }            
+  
---
+++ traversarray.c
@@ -0,0 +1,24 @@
+// input array = {10,20,30,40,50,60,70,80,90};
+// output = 10
+//          20 
+//          30 
+//          40 
+//          50
+//          60
+//          70
+//          80
+//          90
+
+#include<stdio.h>
+int main(){
+    int arr[20] = {1,2,3,4,5,6,7,8,9,0};
+    int i,n;
+    // printf("enter number of element");
+    // scanf("%d",&n);
+    
+    for ( i = 0; i < 10; i++)
+    {
+      printf("%d",arr[i]);  
+    }
+ return 0;   
+}      
