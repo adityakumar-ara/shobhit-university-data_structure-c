885100152ba97dc371817cf5797d69bbf0e21b84
Aditya Kumar <adityaara7667@gmail.com>
2026-02-04 22:04:46 +0530

    add more

--- maxmin.c
+++ maxmin.c
@@ -29,34 +29,34 @@
 //     return 0;
 // }
 
-#include<stdio.h>
-int main(){
-    int arr[20],i,n,max,min;
-    printf("Enter number of element");
-    scanf("%d",&n);
-    for ( i = 0; i < n; i++)
-    {
-        printf("Enter elements of array:");
-        scanf("%d",&arr[i]);
-    }
-    max = arr[0];
-    min = arr[0];
-    for ( i = 0; i < n; i++)
-    {
-        if (arr[i]> max)
-        {
-            max = arr[i];
-        }
+// #include<stdio.h>
+// int main(){
+//     int arr[20],i,n,max,min;
+//     printf("Enter number of element");
+//     scanf("%d",&n);
+//     for ( i = 0; i < n; i++)
+//     {
+//         printf("Enter elements of array:");
+//         scanf("%d",&arr[i]);
+//     }
+//     max = arr[0];
+//     min = arr[0];
+//     for ( i = 0; i < n; i++)
+//     {
+//         if (arr[i]> max)
+//         {
+//             max = arr[i];
+//         }
         
-    }
-    for ( i = 0; i < n; i++)
-    {
-        if (arr[i]< min)
-        {
-            min = arr[i];
-        }
+//     }
+//     for ( i = 0; i < n; i++)
+//     {
+//         if (arr[i]< min)
+//         {
+//             min = arr[i];
+//         }
         
-    }
-    printf("maximum number is :%d \n",max);
-    printf("minimun number is :%d \n",min);
-}
+//     }
+//     printf("maximum number is :%d \n",max);
+//     printf("minimun number is :%d \n",min);
+// }
