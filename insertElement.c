2c09c6dcbbeb2d7db9bfe2eeb9cd92b2e38a5649
Aditya Kumar <adityaara7667@gmail.com>
2026-02-04 19:08:38 +0530

    add more

---
+++ insertElement.c
@@ -0,0 +1,31 @@
+// Write a program to insert a element at specific position
+
+#include<stdio.h>
+int main(){
+    int arr[20];
+    int n,pos,i,x;
+    printf("Enter NUmber of element:");
+    scanf("%d",&n);
+    for ( i = 0; i < n; i++)
+    {
+        printf("Enter elemnt:");
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter new element");
+    scanf("%d",&x);
+    
+    printf("Enter position");
+    scanf("%d",&pos);
+    n+=1;
+    for ( i = n; i >= pos; i--)
+    {
+        arr[i] = arr[i-1];
+    }
+    arr[pos-1] = x;
+    for ( i = 0; i < n; i++)
+    {
+        printf("%d",arr[i]);
+    }
+  return 0;  
+    
+}
--- maxmin.c
+++ maxmin.c
@@ -1,30 +1,62 @@
 // write a program to c find maximum and minimum number in array
 
 
+// #include<stdio.h>
+// int main(){
+//     int arr[20],max,min,i,n;
+//     printf("enter number of element:");
+//     scanf("%d",&n);
+//     for ( i = 0; i < n; i++)
+//     {
+//         printf("enter element:");
+//         scanf("%d",&arr[i]);
+//     }
+//     max = arr[0];
+//     min = arr[0];
+//     for (i = 1; i < n; i++) {
+//         // Check for new maximum
+//         if (arr[i] > max) {
+//             max = arr[i];
+//         }
+
+//         // Check for new minimum
+//         if (arr[i] < min) {
+//             min = arr[i];
+//         }
+//     }
+//     printf("\n maximum number is :%d",max);
+//     printf("\n minimum number is :%d",min);
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
-    int arr[20],max,min,i,n;
-    printf("enter number of element:");
+    int arr[20],i,n,max,min;
+    printf("Enter number of element");
     scanf("%d",&n);
     for ( i = 0; i < n; i++)
     {
-        printf("enter element:");
+        printf("Enter elements of array:");
         scanf("%d",&arr[i]);
     }
     max = arr[0];
     min = arr[0];
-    for (i = 1; i < n; i++) {
-        // Check for new maximum
-        if (arr[i] > max) {
+    for ( i = 0; i < n; i++)
+    {
+        if (arr[i]> max)
+        {
             max = arr[i];
         }
-
-        // Check for new minimum
-        if (arr[i] < min) {
+        
+    }
+    for ( i = 0; i < n; i++)
+    {
+        if (arr[i]< min)
+        {
             min = arr[i];
         }
+        
     }
-    printf("\n maximum number is :%d",max);
-    printf("\n minimum number is :%d",min);
-    return 0;
+    printf("maximum number is :%d \n",max);
+    printf("minimun number is :%d \n",min);
 }
