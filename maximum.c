22750eb06293c54fd7845a887a2709a09bdcc0f6
Aditya Kumar <adityaara7667@gmail.com>
2026-02-04 22:28:22 +0530

    add more

--- maximum.c
+++ maximum.c
@@ -1,23 +1,23 @@
-#include<stdio.h>
-int main (){
-    int arr[20],i,n, max;
-    printf("ener number of element:");
-    scanf("%d",&n);
-    for ( i = 0; i < n; i++)
-    {
-        printf("enter element:");
-        scanf("%d",&arr[i]);
-    }
-    max = arr[0];
-    for ( i = 0; i < n; i++)
-    {
-        if (arr[i]> max)
-        {
-           max = arr[i];
-        }
+// #include<stdio.h>
+// int main (){
+//     int arr[20],i,n, max;
+//     printf("ener number of element:");
+//     scanf("%d",&n);
+//     for ( i = 0; i < n; i++)
+//     {
+//         printf("enter element:");
+//         scanf("%d",&arr[i]);
+//     }
+//     max = arr[0];
+//     for ( i = 0; i < n; i++)
+//     {
+//         if (arr[i]> max)
+//         {
+//            max = arr[i];
+//         }
        
-    }
-    printf("%d",max); 
-    return 0;
+//     }
+//     printf("%d",max); 
+//     return 0;
     
-}
+// }
--- minimum.c
+++ minimum.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
-int main(){
-    int arr[20],n,i,min;
-    printf("Enter number of element:");
-    scanf("%d",&n);
-    for ( i = 0; i < n; i++)
-    {
-        printf("enter element:");
-        scanf("%d",&arr[i]);
-    }
-    min = arr[0];
-    for ( i = 0; i < n; i++)
-    {
-        if (arr[i]<min)
-        {
-            min = arr[i];
-        }
+// #include<stdio.h>
+// int main(){
+//     int arr[20],n,i,min;
+//     printf("Enter number of element:");
+//     scanf("%d",&n);
+//     for ( i = 0; i < n; i++)
+//     {
+//         printf("enter element:");
+//         scanf("%d",&arr[i]);
+//     }
+//     min = arr[0];
+//     for ( i = 0; i < n; i++)
+//     {
+//         if (arr[i]<min)
+//         {
+//             min = arr[i];
+//         }
         
-    }
-  printf("%d",min);
-  return 0;   
-}
+//     }
+//   printf("%d",min);
+//   return 0;   
+// }
