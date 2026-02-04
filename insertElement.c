ffdf8ef7998a0f9fa17258f7e362e382046014a6
Aditya Kumar <adityaara7667@gmail.com>
2026-02-04 21:13:44 +0530

    add more

--- insertElement.c
+++ insertElement.c
@@ -30,29 +30,29 @@
     
 // }
 
-#include<stdio.h>
-  int main(){
-  int arr[20],n,x,pos,i;
-  printf("Enter number of element");
-  scanf("%d",&n);
-  for ( i = 0; i < n; i++)
-  {
-    printf("Enter element:");
-    scanf("%d",&arr[i]);
-  }
-  printf("Enter new number:");
-  scanf("%d",&x);
-  printf("Enter position:");
-  scanf("%d",&pos);
-  n+=1;
-  for ( i = n; i >= pos; i--)
-  {
-    arr[i] = arr[i-1];
-  }
-  arr[pos-1] = x;
-  for ( i = 0; i < n; i++)
-  {
-    printf("%d",arr[i]);
-  }
- return 0;
-}
+// #include<stdio.h>
+//   int main(){
+//   int arr[20],n,x,pos,i;
+//   printf("Enter number of element");
+//   scanf("%d",&n);
+//   for ( i = 0; i < n; i++)
+//   {
+//     printf("Enter element:");
+//     scanf("%d",&arr[i]);
+//   }
+//   printf("Enter new number:");
+//   scanf("%d",&x);
+//   printf("Enter position:");
+//   scanf("%d",&pos);
+//   n+=1;
+//   for ( i = n; i >= pos; i--)
+//   {
+//     arr[i] = arr[i-1];
+//   }
+//   arr[pos-1] = x;
+//   for ( i = 0; i < n; i++)
+//   {
+//     printf("%d",arr[i]);
+//   }
+//  return 0;
+// }
