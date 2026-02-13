31ab1fff4c86c551e7947520631377e87429572b
Aditya Kumar <adityaara7667@gmail.com>
2026-02-13 15:18:43 +0530

    add more

---
+++ lab/array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int arr[20],i,n;
+    printf("enter nuber of elements:");
+    scanf("%d",&n);
+    printf("enter elements");
+    for ( i = 0; i < n; i++)
+    {
+      scanf("%d",&arr[i]); 
+    }
+    for ( i = 0; i <n; i++)
+    {
+       printf("%d\n",arr[i]);
+    }
+    
+   
+   return 0;
+}
