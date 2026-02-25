aec42641b66e0604ebbe0afe7663385e81c959b0
Aditya Kumar <adityaara7667@gmail.com>
2026-02-25 08:10:27 +0530

    update

--- lab/binarysearch.c
+++ lab/binarysearch.c
@@ -1,49 +1,65 @@
-#include <stdio.h>
+// #include <stdio.h>
+// int main()
+// {
+//     int a[7] = {1, 3, 2, 5, 4, 8, 6};
+//     for (int i = 0; i < 7; i++)
+//     {
+//         for ( int j = i + 1; j < 7; j++)
+//         {
+//             if (a[i] > a[j])
+//             {
+//                 int temp = a[i];
+//                 a[i] = a[j];
+//                 a[j] = temp;
+//             }
+//         }
+//     }    
+//     // for ( int i = 0; i < 7; i++)
+//     //     {
+//     //         printf("%d", a[i]);
+//     //     }
+//     // -----------------------------------------------------------------------------------------------
+//     int start = 0, end = 6, mid, n;
+//     int found = 0;
+
+//     printf("Enter number: ");
+//     scanf("%d", &n);
+//     while (start <= end) {
+//         mid = (start + end) / 2; 
+
+//         if (a[mid] == n) { 
+        
+//             printf("Found your number at index: %d\n", mid);
+//             found = 1;
+//             break;
+//         } else if (a[mid] < n) {
+            
+//             start = mid + 1;
+//         } else {
+            
+//             end = mid - 1;
+//         }
+//     }
+
+//     if (found == 0) {
+//         printf("Number not found in the array.\n");
+//     }
+    
+//     return 0;     
+// }  
+
+
+#include<stdio.h>
 int main()
 {
-    int a[7] = {1, 3, 2, 5, 4, 8, 6};
-    for (int i = 0; i < 7; i++)
+    int i , first , last , middle , n , search , arr[100];
+    printf("enter number of elements");
+    scanf("%d",&n);
+    printf("enter elements");
+    for ( i = 0; i < n; i++)
     {
-        for ( int j = i + 1; j < 7; j++)
-        {
-            if (a[i] > a[j])
-            {
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            }
-        }
-    }    
-    for ( int i = 0; i < 7; i++)
-        {
-            printf("%d", a[i]);
-        }
-    // -----------------------------------------------------------------------------------------------
-    int start = 0, end = 6, mid, n;
-    int found = 0;
-
-    printf("Enter number: ");
-    scanf("%d", &n);
-    while (start <= end) {
-        mid = (start + end) / 2; 
-
-        if (a[mid] == n) { 
-        
-            printf("Found your number at index: %d\n", mid);
-            found = 1;
-            break;
-        } else if (a[mid] < n) {
-            
-            start = mid + 1;
-        } else {
-            
-            end = mid - 1;
-        }
+        scanf("%d",&arr[i]);
     }
-
-    if (found == 0) {
-        printf("Number not found in the array.\n");
-    }
-    
-    return 0;     
-}  
+    printf("\nenter value to find:"); 
+    scanf("%d",&search);
+}
---
+++ lab/bubbleshort.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a[10] = {1,3,5,2,4,6,8,7,9,10};
+    for (int i = 0; i < 10; i++)
+    {
+         
+    }
+    
+}
