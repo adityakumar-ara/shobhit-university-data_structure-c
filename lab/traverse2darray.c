c75f2e8e3641d67de3a539c8b81cb822b2b5cef4
Aditya Kumar <adityaara7667@gmail.com>
2026-03-06 15:43:04 +0530

    add more

---
+++ lab/traverse2darray.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int arr[2][2],i,j;
+    printf("enter elements:");
+    for ( i = 0; i < 2; i++)
+    {
+        for ( j = 0; j < 2; j++)
+        {
+            scanf("%d",&arr[i][j]);
+    
+        }
+        
+    }
+    for ( i = 0; i < 2; i++)
+    {
+        for ( j = 0; j < 2; j++)
+        {
+            printf("%d",arr[i][j]);
+        }
+      printf(" \n ");
+    }
+    return 0;
+   
+}
