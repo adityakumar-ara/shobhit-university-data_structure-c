3c94e28f96b2eca5ece03b01b79010d8a2c99b27
Aditya Kumar <adityaara7667@gmail.com>
2026-03-13 22:37:02 +0530

    add more

---
+++ lab/Upper_Triangular.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int a[3][3],i,j;
+    for ( i = 0; i < 3; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            printf("Enter element for Row %d , column %d:",i,j);
+            scanf("%d",&a[i][j]);
+        }
+          
+    }
+    
+    for ( i = 0; i < 3; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            printf("%d",a[i][j]);
+        } 
+        printf("\n\n");       
+    }
+    
+}
