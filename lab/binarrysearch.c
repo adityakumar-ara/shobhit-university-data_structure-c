60e89bedd8e16eac224c2728748d30bb8d188808
Aditya Kumar <adityaara7667@gmail.com>
2026-03-14 12:46:11 +0530

    add more

--- lab/binarrysearch.c
+++ lab/binarrysearch.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
 int main(){
     int arr[]={10,20,30,40,50};
-    int lr=0,up=4,f=0;
+    int lr=0,up=4,f=0,mid;
     int search;
-    print("enter number who want to serach:");
+    printf("enter number who want to serach:");
     scanf("%d",&search);
     while(lr<=up){
-          int mid = (lr+up)/2;
+          mid = (lr+up)/2;
+          if (arr[mid]==search)
+          {
+            f=1;
+            break;
+          }
+          else if (arr[mid]<search)
+          {
+            lr=mid+=1;
+          }
+          else
+          up = mid-1;
     }
+    if (f==1)
+    {
+        printf("search number is found at index:%d",mid);
+    }
+    else
+    {
+        printf("not found");
+    }
+   return 0; 
 }
